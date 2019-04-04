bb8521939e4849761af0a9befdc8ac80c3707821
Akshay Krishnan <akshay@Kirans-MacBook-Pro.local>
2019-04-05 00:47:36 +0530

    Mutation with onCompleted

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,35 +2,26 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 
+
 export default class Login extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        email: '',
-        password: ''
-    };
-
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    //this.handleLogin = this.handleLogin.bind(this);
+  state = {
+    email: '',
+    password: '',
   }
 
-  handleEmailChange(event) {
-    this.setState({ email: event.target.value });
-    console.log("Hello");
-  }
-
-  handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
+  handleLogin = async data => {
+    const { token } = data.tokenCreate;
+    localStorage.setItem("authToken", token);
   }
 
   render() {
 
+    const { email, password } = this.state
+
     const GET_AUTH_TOKEN = gql(`
-      mutation {
-        tokenCreate(email: "${this.state.email}", password: "${this.state.password}") {
+      mutation LoginMutation($email: String!, $password: String!) {
+        tokenCreate(email: $email, password: $password) {
           token,
           errors {
             message 
@@ -40,28 +31,20 @@ export default class Login extends Component {
     `);
 
     return (
-        <Mutation mutation={GET_AUTH_TOKEN}>
-          {(tokenCreate, { data }) => {
-            // if (data) {
-            //   this.props.loginAs(data);
-            // }
-            return ( 
-            <div>
-              <form onSubmit={event => {
-                event.preventDefault();
-                tokenCreate();
-              }}>
-                <input type="text" placeholder="email" value={this.state.email} onChange={this.handleEmailChange} />
-                <input type="password" placeholder="password" value={this.state.password} onChange={this.handlePasswordChange} />
-                <button type="submit">LOGIN</button>
-                {data && data.tokenCreate.errors[0].message && <p>Oops</p>}
-              </form>
-            </div>
-            )
-          }}
-        </Mutation>
+      <div>
+        <input type="text" placeholder="email" value={email} onChange={e => this.setState({ email: e.target.value })} />
+        <input type="password" placeholder="password" value={password} onChange={e => this.setState({ password: e.target.value })} />
 
-      
-    );
+        <Mutation
+          mutation={GET_AUTH_TOKEN}
+          variables={{ email, password }}
+          onCompleted={(data) => this.handleLogin(data)}
+        >
+          { tokenCreate => (
+            <a onClick={tokenCreate}>Login</a>
+          )}
+        </Mutation>
+      </div>
+    )
   }
 }
