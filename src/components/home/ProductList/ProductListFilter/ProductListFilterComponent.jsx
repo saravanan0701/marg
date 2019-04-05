218a5a5935c3b0982bb97f936360f7b597ca641e
uday-r <uday@1stmain.co>
2019-04-05 13:22:07 +0530

    1. Added reactstrap which is based on bootstrap-4. 2. Converted `Filter` to stateful component as it needs to toggle content visibility. Using constructor bind to trigger state-change, this component is re-rendered we open/collapse so constructor bind is a better solution.

--- src/components/home/ProductList/ProductListFilter/ProductListFilterComponent.jsx
+++ src/components/home/ProductList/ProductListFilter/ProductListFilterComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Query } from "react-apollo";
 import gql from 'graphql-tag';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
 const LOAD_FILTERS = gql`
   query LoadFilters {
@@ -30,16 +31,53 @@ const LOAD_FILTERS = gql`
   }
 `;
 
-const Filter = ({ filterName, filters, noFilters }) => (
+class Filter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: typeof(props.isOpen) !== 'undefined'? props.isOpen: false,
+    }
+    this.toggleIsOpen = this.toggleIsOpen.bind(this);
+  }
+
+  toggleIsOpen() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+
+  render() {
+    const {
+      isOpen,
+    } = this.state;
+    const {
+      noFilters,
+      filterName,
+      children
+    } = this.props;
+    return (
+      <div>
+        <h1 onClick={this.toggleIsOpen}>{ filterName }</h1>
+        <Collapse isOpen={isOpen}>
+          {!children && <div>{noFilters? noFilters: 'No Filter found'}</div>}
+          {children}
+        </Collapse>
+      </div>
+    )
+  }
+}
+
+const FilterListRepeater = ({ filters }) => (
   <div>
-    <h1>{ filterName }</h1>
-    {(!filters || filters.length == 0) && <div>{noFilters? noFilters: 'No Filter found'}</div>}
     {
       (filters && filters.length > 0) && (
-        filters.map((category) => (
-            <div key={category.slug}>{category.name}</div>
-          )
-        )
+        filters.map((filter) => ( 
+          <div key = {
+            filter.slug
+          } > {
+            filter.name
+          }</div>
+        ))
       )
     }
   </div>
@@ -49,7 +87,7 @@ export const ProductListFilter = props => (
   <Query query={LOAD_FILTERS}>
     {
       ({loading, error, data}) => {
-        if(typeof(error) === "undefined") {
+        if(typeof(error) !== "undefined") {
           //TODO: error handling...
         }
         if(loading) {
@@ -64,13 +102,19 @@ export const ProductListFilter = props => (
             edges: categoryEdges,
           }
         } = data;
+        // const editorEdges = data.attributes.edges;
+        // TODO: to be removed.
         const editors = editorEdges[0].node.values;
         const categories = categoryEdges.reduce((acc, it) => (acc.concat([it.node])), [])
 
         return <div>
           <h1>All Filters</h1>
-          <Filter filterName="Categories" filters={categories} noFilters="No Categories Found" />
-          <Filter filterName="Editors" filters={editors} noFilters="No Editors Found" />
+          <Filter filterName="Categories" noFilters="No Categories Found">
+            <FilterListRepeater filters={categories} />
+          </Filter>
+          <Filter filterName="Editors" noFilters="No Editors Found">
+            <FilterListRepeater filters={editors} />
+          </Filter>
         </div>;
       }
     }
