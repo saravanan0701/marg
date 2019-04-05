3529c1b40d9e0306389429169d6d9307bf30a6bb
uday-r <uday@1stmain.co>
2019-04-05 13:12:14 +0530

    Adding ProductList on Home component.

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProductList from './ProductList/';
 
 export const Home = props => (
   <div>
     <h1>Home</h1>
+    <ProductList />
     <button onClick={() => {
       props.history.push('/login')
     }} href="/login">LOGIN</button>
