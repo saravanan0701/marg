898816476c6ab990fe9ea472614bd1a4378979bf
uday-r <uday@1stmain.co>
2019-04-08 11:37:19 +0530

    Moved `/components/home` to `/components/Home`.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
 
 import './App.scss';
-import { Home } from './components/home';
+import { Home } from './components/Home';
 import Auth from './components/auth';
 import client from './apollo';
 import { StoreFactory } from './redux';
