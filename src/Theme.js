fc57321d123ee2dad4340cc1af887a6b8a5bff34
uday-r <uday@1stmain.co>
2019-05-15 17:41:41 +0530

    Updated theme.

--- src/Theme.js
+++ src/Theme.js
@@ -2,7 +2,8 @@ const Theme = {
   primaryColor: "#ec1d24",
   primaryHoverColor: "#ec1d24",
   primaryHoverUnderLineColor: "#ec1d24",
-  secondaryColor: "grey",
+  secondaryColor: "#3a3a3a",
+  secondaryHoverColor: "#ec1d24",
 }
 
 export default Theme;
