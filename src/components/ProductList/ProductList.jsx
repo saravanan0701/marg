ff643e1803228d49bb9355d2a34b5659684339e7
uday-r <uday@1stmain.co>
2019-05-17 16:24:18 +0530

    Added static headers.

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import ProductListFilter from './ProductListFilter';
 import ProductListWrapper from './ProductListWrapper';
+import { FlatButton } from './../commons/FlatButton.jsx';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 100px 150px;
+
+  & > .heading {
+    font-family: "Cormorant Garamond Medium";
+    font-size: ${props => props.theme['$font-size-lg']};
+    font-weight: ${props => props.theme['$weight-regular']};
+    letter-spacing: 1px;
+    line-height: 42px;
+    text-align: center;
+  }
+
+  & > .product-types {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-top: 40px;
+    padding-bottom: 30px;
+
+    & > div {
+      margin-left: 50px;
+    }
+  }
+`;
 
 export const ProductList = (props) => (
-  <div className="row">
-    <ProductListFilter className="col-3" />
-    <ProductListWrapper className="col-9" />
-  </div>
+  <Wrapper>
+    <div className="heading">All Publications</div>
+    <div className="product-types">
+      <FlatButton type="secondary">View All</FlatButton>
+      <FlatButton type="secondary">Magazines</FlatButton>
+      <FlatButton type="secondary">Books</FlatButton>
+      <FlatButton type="secondary">Digital Archives</FlatButton>
+    </div>
+    <ProductListFilter />
+    <ProductListWrapper />
+  </Wrapper>
 );
