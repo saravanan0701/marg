8a7ba6c3b9cd93f1ab7a28afeb98a9fff9ece155
uday-r <uday@1stmain.co>
2019-05-25 17:41:27 +0530

    Loading backend categories to display.

--- src/components/ProductList/ProductCategoryFilter/ProductCategoryFilterComponent.jsx
+++ src/components/ProductList/ProductCategoryFilter/ProductCategoryFilterComponent.jsx
@@ -18,38 +18,60 @@ const Wrapper = styled.div`
       border-bottom: 1px solid ${props => props.theme.primaryColor};
     }
   }
+`;
 
+const LOAD_CATEGORIES = gql`
+  query LoadCategories {
+    categories(query: "") {
+      edges {
+        node {
+          id
+          name
+          slug
+        }
+      }
+    }
+  }
 `;
 
 export const ProductCategoryFilterComponent = ({ category, selectCategory }) => (
   <Wrapper>
-    <FlatButton
-      onMouseDown={() => selectCategory(null)}
-      className={!category? 'active': ''}
-      type="secondary"
-      >
-      View All
-    </FlatButton>
-    <FlatButton
-      onMouseDown={() => selectCategory('magazines')}
-      className={category === 'magazines'? 'active': ''}
-      type="secondary"
-      >
-      Magazines
-    </FlatButton>
-    <FlatButton
-      onMouseDown={() => selectCategory('books')}
-      className={category === 'books'? 'active': ''}
-      type="secondary"
-      >
-      Books
-    </FlatButton>
-    <FlatButton
-      onMouseDown={() => selectCategory('digital-archives')}
-      className={category === 'digital-archives'? 'active': ''}
-      type="secondary"
-      >
-      Digital Archives
-    </FlatButton>
+    <Query query={LOAD_CATEGORIES}>
+      {
+      ({loading, data, error}) => {
+            if(loading) {
+              return <h4>Loading Categories, please wait</h4>
+            }
+            if(error) {
+              return <h4>Error loading categories</h4>
+            }
+            let menus = [<FlatButton
+              key="0"
+              onMouseDown={() => selectCategory(null)}
+              className={!category? 'active': ''}
+              type="secondary"
+              >
+              View All
+            </FlatButton>];
+            data.categories.edges.forEach(
+              (categoryIt, id) => menus.push(
+                <FlatButton key={id + 1}
+                  onMouseDown={() => selectCategory({
+                    name: categoryIt.node.name,
+                    slug: categoryIt.node.slug,
+                    id: categoryIt.node.id,
+                  })}
+                  className={category && category.slug === categoryIt.node.slug? 'active': ''}
+                  type="secondary"
+                >
+                  {categoryIt.node.name}
+                </FlatButton>
+              )
+            );
+
+            return menus.map((menu) => menu);
+          }
+        }
+    </Query>
   </Wrapper>
 );
