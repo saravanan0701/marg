8176c2030dc53b63220141e781bee9038695042b
uday-r <uday@1stmain.co>
2019-05-31 15:18:22 +0530

    `No products found` message added in ProductsListWrapper. Indentation changes.

--- src/components/ProductList/ProductListWrapper/ProductListWrapper.jsx
+++ src/components/ProductList/ProductListWrapper/ProductListWrapper.jsx
@@ -62,16 +62,29 @@ export default class ProductListWrapper extends Component {
     return (
       <Wrapper className={`row`}>
         {
-          !loadProductsError && loadingAllProducts && <h3>Loading products please wait(TO be changed).....</h3>
+          !loadProductsError
+            && loadingAllProducts
+            && <h3>Loading products please wait(TO be changed).....</h3>
         }
         {
-          !loadProductsError && !loadingAllProducts && products.map((product) => (<ProductCard className="col-4" {...product} />))
+          !loadProductsError
+            && !loadingAllProducts
+            && products.map((product) => (<ProductCard className="col-4" {...product} />))
         }
         {
-          !loadProductsError && loadingNextPage && <h3>Loading more products</h3>
+          !loadProductsError
+            && !loadingAllProducts
+            && (!products || (products && products.length == 0))
+            && <h3>No products found</h3>
         }
         {
-          loadProductsError && <h3>Error loading page, please refresh</h3>
+          !loadProductsError
+            && loadingNextPage
+            && <h3>Loading more products</h3>
+        }
+        {
+          loadProductsError
+            && <h3>Error loading page, please refresh</h3>
         }
       </Wrapper>
     )
