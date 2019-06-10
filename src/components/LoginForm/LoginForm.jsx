f67fbaa66312c93aa75291e677f30230934bb8db
uday-r <uday@1stmain.co>
2019-06-10 18:15:35 +0530

    Cleaned login form.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
+import styled from 'styled-components';
+import { FlatButton, RaisedButton } from './../commons/';
 
 
 const GET_AUTH_TOKEN = gql(`
@@ -13,6 +15,63 @@ const GET_AUTH_TOKEN = gql(`
     }
   }
 `);
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 100px 100px;
+
+  & > div{
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+
+    & > .heading {
+      color: #000000;
+      font-family: "Cormorant Garamond Medium";
+      font-size: ${props => props.theme['$font-size-lg']};
+      font-weight: ${props => props.theme['$weight-bold']};
+      letter-spacing: 1px;
+      line-height: 57px;
+      padding-bottom: 30px;
+    }
+
+    & > .label {
+      color: #000000;
+      font-size: ${props => props.theme['$font-size-xxs']};
+      font-weight: ${props => props.theme['$weight-bold']};
+      letter-spacing: 3px;
+      text-transform: uppercase;
+      padding-bottom: 15px;
+      padding-top: 50px;
+    }
+
+    & > input {
+      border: 1px solid #cccccc;
+      padding: 10px;
+    }
+
+    & > .forgot-button {
+      align-self: flex-end;
+      padding-top: 10px;
+      padding-right: 0px !important;
+    }
+
+    & > .login-button {
+      align-self: flex-start;
+    }
+
+    & > .signup {
+      padding-top: 50px;
+      display: flex;
+      flex-direction: row;
+
+      & > div.button {
+        padding-left: 10px;
+      }
+    }
+  }
+`;
 
 export default class Login extends Component {
 
@@ -62,26 +121,33 @@ export default class Login extends Component {
     const { email, password } = this.state
 
     return (
-      <div>
-
-        <input type="text" placeholder="email" value={email} onChange={e => this.setState({ email: e.target.value })} />
-        <input type="password" placeholder="password" value={password} onChange={e => this.setState({ password: e.target.value })} />
-
-        <Mutation
-          mutation={GET_AUTH_TOKEN}
-          variables={{ email, password }}
-          onCompleted={(data) => this.handleLogin(data)}
-        >
-          {
-            (tokenCreate, { loading }) => (
-              <div>
-                <button onClick={tokenCreate}>{ loading ? 'Logging in...' : 'Login'}</button>
-              </div>
-            )
-          }
-        </Mutation>
-
-      </div>
+      <Wrapper>
+        <div>
+          <div className="heading">Sign In</div>
+          <div className="label">Email</div>
+          <input type="text" placeholder="email" value={email} onChange={e => this.setState({ email: e.target.value })} />
+          <div className="label">Password</div>
+          <input type="password" placeholder="password" value={password} onChange={e => this.setState({ password: e.target.value })} />
+          <FlatButton className="forgot-button">Forgot password</FlatButton>
+          <Mutation
+            mutation={GET_AUTH_TOKEN}
+            variables={{ email, password }}
+            onCompleted={(data) => this.handleLogin(data)}
+          >
+            {
+              (tokenCreate, { loading }) => (
+                <div className="login-button">
+                  <RaisedButton onClick={tokenCreate}>{ loading ? 'Logging in...' : 'Sign in'}</RaisedButton>
+                </div>
+              )
+            }
+          </Mutation>
+          <div className="signup">
+            <div>Don’t have an account? </div>
+            <FlatButton className="button">Register</FlatButton>
+          </div>
+        </div>
+      </Wrapper>
     )
   }
 }
