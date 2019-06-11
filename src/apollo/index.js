c640d88e601feae784aea84b80ed4572901b5bca
uday-r <uday@1stmain.co>
2019-06-11 12:43:25 +0530

    Adding headers in every graphql request.

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,15 +1,28 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 
-
-const link = createHttpLink({
+const httpLink = createHttpLink({
   uri: process.env.REACT_APP_BACKEND_URL
 });
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('authToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `JWT ${token}` : "",
+    }
+  }
+});
+
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link,
+  link: authLink.concat(httpLink),
   cache,
 });
 
