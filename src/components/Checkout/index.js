722144133a95ab700a382fca9be803f2ce6e8516
uday-r <uday@1stmain.co>
2019-07-11 22:24:17 +0530

    Adding missing route.

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import CheckoutAddress from './CheckoutAddress/';
 import CheckoutBag from './CheckoutBag/';
+import CheckoutPayment from './CheckoutPayment/';
 
 class Checkout extends Component {
 
@@ -12,11 +13,19 @@ class Checkout extends Component {
     };
   }
 
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div>
         <Route exact path="/checkout/" component={CheckoutBag} />
         <Route exact path="/checkout/address/" component={CheckoutAddress} />
+        <Route exact path="/checkout/payment/" component={CheckoutPayment} />
       </div>
     )
   }
