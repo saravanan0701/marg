98ef2c4a012048eb4b8922c32f2b5b3c8efd6842
Akshay <akshaykay@gmail.com>
2019-07-17 09:49:00 +0530

    Footer responsiveness complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 import FontAwesome from 'react-fontawesome';
 import styled from 'styled-components';
-import logo from './../../images/logo.png'
-import headerImg from './../../images/header-left.jpg'
+import logo from './../../images/logo.png';
+import headerImg from './../../images/header-left.jpg';
 
+import { Link } from 'react-router-dom';
 import { FlatButton, RaisedButton } from './../commons/';
 import { Container, Row, Col } from 'reactstrap';
 
@@ -14,197 +15,124 @@ const FooterContainer = styled.div`
   flex-direction: column;
 
   .footer-link {
-
     font-size: $font-size-xxs;
     letter-spacing: 3px;
     text-transform: uppercase;
     margin-bottom: 1.5rem;
 
-    span {
-      color: red;
-    }
-    
+    span { color: red; }
   }
 
-  & > .primary {
-    padding-top: 20px;
-    height: 25vh;
-    background-color: #cccccc;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+  .subscribe-heading {
+    color: #000000;
+    font-family: "Cormorant Garamond Medium";
 
-    & > .img-container {
-      height: 100%;
-      width: 50%;
-      max-width: 50%;
-      margin-right: 10px;
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-end;
-      & > img {
-        height: 100%;
-        margin-right: 15%;
-      }
-    }
+    font-size: ${props => props.theme['$font-size-sm']};
+    @media (min-width: 992px) { font-size: ${props => props.theme['$font-size-md']}; }
+    @media (min-width: 1200px) { font-size: ${props => props.theme['$font-size-lg']}; }
 
-    & > .text-container {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      width: 50%;
-      max-width: 50%;
-
-      & > .text {
-        width: 50%;
-      }
-    }
+    font-weight: ${props => props.theme['$weight-regular']};
+    letter-spacing: 1px;
   }
 
-  & > .secondary {
-    padding-top: 20px;
+  input.subscribe {
+    border: 1px solid #cccccc;
     background-color: #f8f8f8;
-    display: flex;
-    flex-direction: row;
-    padding: 60px 40px 40px;
+    padding: 10px;
+    margin-right: 10px;
+    width: 50%;
 
-    & > div {
-      width: 50%;
+    &::placeholder {
+      color: ${props => props.theme[props.type ? `${props.type}Color` : "primaryColor"]};
+      font-size: ${props => props.theme['$font-size-xxxs']};
+      font-weight: ${props => props.theme['$weight-bold']};
+      letter-spacing: 3px;
+      text-transform: uppercase;
     }
+  }
 
-    & > .subscribe {
-      display: flex;
-      flex-direction: column;
-      width: 50%;
-
-      & > .heading {
-        color: #000000;
-        font-family: "Cormorant Garamond Medium";
-        font-size: ${props => props.theme['$font-size-lg']};
-        font-weight: ${props => props.theme['$weight-regular']};
-        letter-spacing: 1px;
-        line-height: 57px;
-      }
-
-      & > .form {
-        display: flex;
-        flex-direction: row;
-
-        & > input {
-          border: 1px solid #cccccc;
-          background-color: #f8f8f8;
-          padding: 10px;
-          margin-right: 10px;
-
-          &::placeholder {
-            color: ${props => props.theme[props.type ? `${props.type}Color` : "primaryColor"]};
-            font-size: ${props => props.theme['$font-size-xxs']};
-            font-weight: ${props => props.theme['$weight-bold']};
-            letter-spacing: 3px;
-            text-transform: uppercase;
-          }
-        }
-      }
+  #magazineSubscribe {
+    background-color: #fcf8d1;
+    a {
+      color: ${props => props.theme["primaryColor"]};
+      font-family: Lato;
+      font-size: ${props => props.theme['$font-size-xxs']};
+      font-weight: ${props => props.theme['$weight-bold']};
+      letter-spacing: 3px;
+      text-transform: uppercase;
     }
-
-    & > .links {
-      display: flex;
-      flex-direction: column;
-      width: 50%;
-
-      & > .hypes {
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        & > div {
-          width: 50%;
-          & > div {
-            width: fit-content;
-            margin-bottom: 25px
-          }
-        }
-      }
-
-      & > .copyright {
-        color: #000000;
-        font-size: 14px;
-        font-weight: ${props => props.theme['$weight-regular']};
-        letter-spacing: 0.52px;
-        line-height: 21px;
-      }
+    p {
+      color: black;
+      font-family: Lato;
+      font-size: ${props => props.theme['$font-size-xxs']};
+      font-weight: ${props => props.theme['$weight-regular']};
+      letter-spacing: 0.59px;
+      line-height: 23px;
+      max-width: 350px;
     }
+  }
+
+  .legal {
+    color: #000000;
+    font-family: Lato;
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: 0.52px;
+    line-height: 21px;
   }
 `
 
 export const Footer = () => (
   <FooterContainer>
-    <Container>
-      <Row>
-        <Col lg='5'>
-          <div className="subscribe">
-            <div className="heading">Sign up for Newsletters:</div>
-            <div className="form">
-              <input type="text" placeholder="Your email" />
-              <RaisedButton colorType="primary">Sign up</RaisedButton>
+    <div id="magazineSubscribe" className='text-center py-5 full-width'>
+      <Link to="/subscribe">SUBSCRIBE TO MARG</Link>
+      <p className="mx-auto my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sem enim. Donec pharetra, sem sed consectetur</p>
+    </div>
+    <div className='py-5 full-width bg-gray'>
+      <Container className="px-4 px-lg-5">
+        <Row>
+          <Col lg='6' className="mb-5 mb-lg-0">
+            <div className="subscribe">
+              <div className="subscribe-heading">Sign up to newsletters:</div>
+              <div className="form my-3">
+                <input type="email" placeholder="Your email" className="subscribe" />
+                <RaisedButton colorType="primary">Sign up</RaisedButton>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col lg='7'>
-          <Row>
-            <Col lg='6'>
-              <FlatButton className="footer-link" colorType="secondary">Home</FlatButton>
-              <FlatButton className='footer-link' colorType="secondary">Publications</FlatButton>
-              <FlatButton className='footer-link' colorType="secondary">About Marg</FlatButton>
-              <FlatButton className='footer-link' colorType="secondary">Blog</FlatButton>
-            </Col>
-            <Col lg='6'>
-              <FlatButton className="footer-link" colorType="secondary">Advertise</FlatButton>
-              <FlatButton className="footer-link" colorType="secondary">Donate</FlatButton>
-              <FlatButton className="footer-link" colorType="secondary">Contact</FlatButton>
-              <FlatButton className="footer-link" colorType="secondary">More</FlatButton>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+          <Col lg='6'>
+            <Row>
+              <Col lg='6'>
+                <FlatButton className="footer-link" colorType="secondary">Home</FlatButton>
+                <FlatButton className='footer-link' colorType="secondary">Publications</FlatButton>
+                <FlatButton className='footer-link' colorType="secondary">About Marg</FlatButton>
+                <FlatButton className='footer-link' colorType="secondary">Blog</FlatButton>
+              </Col>
+              <Col lg='6'>
+                <FlatButton className="footer-link" colorType="secondary">Advertise</FlatButton>
+                <FlatButton className="footer-link" colorType="secondary">Donate</FlatButton>
+                <FlatButton className="footer-link" colorType="secondary">Contact</FlatButton>
+                <FlatButton className="footer-link" colorType="secondary">More</FlatButton>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="my-4">
+          <Col lg="6">
+            <p className="legal">The digitization of the Magazine has been supported by Tata Trusts</p>
+          </Col>
+          <Col lg="6">
+            <span className="legal">
+              <Link to="terms-and-conditions">Terms & Conditions</Link>
+              &nbsp; | &nbsp;
+            <Link to="terms-and-conditions">Privacy Policy</Link>
+              &nbsp; | &nbsp;
+            <p className="d-inline">© 2019 The Marg Foundation</p>
+            </span>
+          </Col>
+        </Row>
+      </Container>
+    </div>
 
-    <div className="primary">
-      <div className="img-container">
-        <img src={headerImg} />
-      </div>
-      <div className="text-container">
-        <FlatButton>Subscribe to Marg</FlatButton>
-        <div className="text">Some footer dummy content for the footer which is a bit
-          long but not long enough to irritate the user</div>
-      </div>
-    </div>
-    <div className="secondary">
-      <div className="subscribe">
-        <div className="heading">Sign up for Newsletters:</div>
-        <div className="form">
-          <input type="text" placeholder="Your email" />
-          <RaisedButton colorType="primary">Sign up</RaisedButton>
-        </div>
-      </div>
-      <div className="links">
-        <div className="hypes">
-          <div>
-            <FlatButton colorType="secondary">Home</FlatButton>
-            <FlatButton colorType="secondary">Publications</FlatButton>
-            <FlatButton colorType="secondary">About Marg</FlatButton>
-            <FlatButton colorType="secondary">Blog</FlatButton>
-          </div>
-          <div>
-            <FlatButton colorType="secondary">Advertise</FlatButton>
-            <FlatButton colorType="secondary">Donate</FlatButton>
-            <FlatButton colorType="secondary">Contact</FlatButton>
-            <FlatButton colorType="secondary">More</FlatButton>
-          </div>
-        </div>
-        <div className="copyright">
-          Terms & Conditions | Privacy Policy | © 2019 The Marg Foundation
-        </div>
-      </div>
-    </div>
   </FooterContainer>
 );
