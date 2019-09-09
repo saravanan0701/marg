6e6c4d7b7ac448d3870e9db1aa93d9495f98d07b
uday <uday@1stmain.co>
2019-09-09 15:27:14 +0530

    FOr guest user cart items are rehyderated from cache.

--- src/redux/auth-sagas.js
+++ src/redux/auth-sagas.js
@@ -12,6 +12,27 @@ const TOKEN_VERIFY = gql(`
   }
 `);
 
+const GET_VARIANTS = gql`
+  query GetVariants($ids: [ID]!, $first: Int){
+    productVariants(ids: $ids, first: $first){
+      edges{
+        node{
+          id
+          isDigital
+          product{
+            name
+            thumbnail{
+              url
+            }
+            images{
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export function* stateRehydrateSaga() {
   yield takeLatest("REHYDERATE_STATE_FROM_CACHE", rehyderateUserFromSession);
@@ -28,6 +49,18 @@ function fetchLoggedInUser(client) {
       token: authToken,
     }
   })
+}
+
+function* listVariants(client, variantIds) {
+  return yield call(
+    () => client.query({
+      query: GET_VARIANTS,
+      variables: {
+        ids: variantIds,
+        first: 100,
+      }
+    })
+  )
 }
 
 function getCountryInfo() {
@@ -59,8 +92,27 @@ function* rehyderateUserFromSession({ client }) {
     
     //if user is not loggedin, cart length is set as `totalQuantity`.
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const variantIds = cart.map(({variantId}) => variantId);
+    const totalQuantity = cart.reduce((acc, {quantity}) => (acc + quantity), 0)
+    let {
+      data: {
+        productVariants: {
+          edges: variantEdges,
+        } = {},
+      } = {},
+    } = yield call(
+      () => listVariants(client, variantIds)
+    )
+    const getVariantQuantity = (id) => {
+      const variant = cart.find(({variantId}) => id === variantId);
+      return variant? variant.quantity: 0;
+    }
+    variantEdges = variantEdges.map(({node}) => ({variant: {...node}, quantity: getVariantQuantity(node.id)}))
     yield put(
-      actions.updateCartQuantity(cart.length)
+      actions.updateCheckoutLines({lines: variantEdges})
+    );
+    yield put(
+      actions.updateCartQuantity(totalQuantity)
     );
   }
   try {
