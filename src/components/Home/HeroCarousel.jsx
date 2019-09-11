f29d2bf431e949f150d7a9667ebcc9bd8ccbdd72
Akshay <akshaykay@gmail.com>
2019-09-11 23:27:40 +0530

    New Header Desktop

--- src/components/Home/HeroCarousel.jsx
+++ src/components/Home/HeroCarousel.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { RaisedButton } from './../commons/';
-import FontAwesome from 'react-fontawesome';
-import Hero1 from '../../images/Magazine Banner Option 1 for Home Page.jpg';
-import Hero2 from '../../images/Contemporary Hindu Temple LR.jpg';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { RaisedButton } from "./../commons/";
+import FontAwesome from "react-fontawesome";
+import Hero1 from "../../images/Magazine Banner Option 1 for Home Page.jpg";
+import Hero2 from "../../images/Contemporary Hindu Temple LR.jpg";
+import { Link } from "react-router-dom";
 import {
   Carousel,
   CarouselItem,
@@ -12,34 +12,36 @@ import {
   Container,
   Row,
   Col
-} from 'reactstrap';
-
+} from "reactstrap";
 
 const items = [
   {
-    positiom: '1',
+    positiom: "1",
     label: "Current Issue",
     title: "Gandhara: A Confluence of Cultures",
     subtitle: "Edited by: Naman P. Ahuja",
-    description: "This issue presents the latest finds from recent excavations and on advances in Gandhara Art  Studies and examines migrations from different periods — from Bronze Age to the movements of Ghaznavids and Mughals.",
+    description:
+      "This issue presents the latest finds from recent excavations and on advances in Gandhara Art  Studies and examines migrations from different periods — from Bronze Age to the movements of Ghaznavids and Mughals.",
     imageSrc: Hero1,
     ctaText: "View This Issue",
-    ctaUrl: "/product/UHJvZHVjdDozMDQz"
+    ctaUrl: "/product/UHJvZHVjdDozMDQz",
+    textColor: '#FFFFFF'
   },
   {
-    position: '2',
+    position: "2",
     label: "QUARTERLY BOOK",
     title: "The Contemporary Hindu Temple: Fragments for a History",
     subtitle: "Edited by: Annapurna Garimella, Shriya Sridharan, A. Srivathsan",
-    description: "This book presents the Hindu temple from the perspectives of institutions and individuals, including priests, building practitioners and worshippers, to consider what it means when the temple is no longer at the centre of Indic life.",
+    description:
+      "This book presents the Hindu temple from the perspectives of institutions and individuals, including priests, building practitioners and worshippers, to consider what it means when the temple is no longer at the centre of Indic life.",
     imageSrc: Hero2,
     ctaText: "View This Book",
-    ctaUrl: "/product/UHJvZHVjdDo1MjUy"
+    ctaUrl: "/product/UHJvZHVjdDo1MjUy",
+    textColor: '#000000'
   }
 ];
 
 const CarouselWrapper = styled.div`
-
   * {
     -webkit-transition: opacity 2s ease-in;
     -moz-transition: opacity 2s ease-in;
@@ -50,7 +52,7 @@ const CarouselWrapper = styled.div`
 
   .carousel-image {
     height: 100%;
-    max-height: 400px !important;
+    max-height: 616px !important;
     width: 100%;
     object-fit: cover;
     object-position: top;
@@ -58,15 +60,19 @@ const CarouselWrapper = styled.div`
 
   #carouselInfoCol {
     padding-bottom: 150px;
-    @media (min-width: ${props => props.theme['mobileBreakpoint']}) {
+    @media (min-width: ${props => props.theme["mobileBreakpoint"]}) {
       min-height: 450px;
     }
+    position: absolute;
+    top: 20%;
+    max-width: 40%;
+    left: 5%;
   }
 
   .controls {
-    position: absolute;
-    bottom: 5%;
-    display: flex;
+    /* position: absolute;
+    bottom: 5%; */
+    display: inline-flex;
     align-items: center;
   }
 
@@ -89,8 +95,8 @@ const CarouselWrapper = styled.div`
       cursor: pointer;
       margin: 0px 5px;
       border-radius: 100%;
-    width: 10px;
-    height: 10px;
+      width: 10px;
+      height: 10px;
     }
   }
 
@@ -101,7 +107,6 @@ const CarouselWrapper = styled.div`
 
   .label {
     display: block;
-    color: #000000;
     font-family: Lato;
     font-size: 13px;
     font-weight: 700;
@@ -116,14 +121,12 @@ const CarouselWrapper = styled.div`
       font-size: 42px;
       line-height: 42px;
     }
-    color: #000000;
     font-family: "Cormorant Garamond Medium";
     font-weight: 500;
     /* min-height: 126px; */
   }
 
   .subtitle {
-    color: #3a3a3a;
     font-family: Lato;
     font-size: 16px;
     font-weight: 400;
@@ -131,12 +134,11 @@ const CarouselWrapper = styled.div`
   }
 
   .description {
-    color: #000000;
     font-family: Lato;
     font-size: 16px;
     font-weight: 400;
     line-height: 23px;
-    @media (min-width: ${props => props.theme['mobileBreakpoint']}) {
+    @media (min-width: ${props => props.theme["mobileBreakpoint"]}) {
       min-height: 92px;
     }
   }
@@ -149,7 +151,6 @@ const CarouselWrapper = styled.div`
       color: white;
     }
   }
-
 `;
 
 class HeroCarousel extends Component {
@@ -173,13 +174,19 @@ class HeroCarousel extends Component {
 
   next() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    const nextIndex =
+      this.state.activeIndex === items.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
 
   previous() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? items.length - 1
+        : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
 
@@ -191,38 +198,54 @@ class HeroCarousel extends Component {
   render() {
     const { activeIndex } = this.state;
 
-    const slides = items.map((item) => {
+    const slides = items.map(item => {
       return (
         <CarouselItem
           onExiting={this.onExiting}
           onExited={this.onExited}
           key={item.position}
         >
-          <Container className="px-3 px-lg-5" fluid="true">
-            <Row>
-              <Col lg="7" className="order-lg-2 px-0">
-                <img className="carousel-image" src={item.imageSrc} alt="" />
-              </Col>
-              <Col id="carouselInfoCol" lg="5">
-                <span className="label my-3">{item.label}</span>
-                <h1 className="title my-2">{item.title}</h1>
-                <h3 className="subtitle mb-4">{item.subtitle}</h3>
-                <p className="description">{item.description}</p>
-                <RaisedButton className="cta"><Link to={item.ctaUrl}>{item.ctaText}</Link></RaisedButton>
-                <div class="controls">
-                  <FontAwesome className="icon" name='chevron-left' onClick={this.previous} />
-                  <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-                  <FontAwesome className="icon" name='chevron-right' onClick={this.next} />
-                </div>
-              </Col>
-            </Row>
+          <Container
+            className="px-0 text-center"
+            fluid="true"
+            style={{
+              position: "relative",
+            }}
+          >
+            <img className="carousel-image" src={item.imageSrc} alt="" />
+            <div id="carouselInfoCol" lg="5" className="text-left" style={{ color: item.textColor }}>
+              <span className="label my-3">{item.label}</span>
+              <h1 className="title my-2">{item.title}</h1>
+              <h3 className="subtitle mb-4">{item.subtitle}</h3>
+              <p className="description">{item.description}</p>
+              <RaisedButton className="cta">
+                <Link to={item.ctaUrl}>{item.ctaText}</Link>
+              </RaisedButton>
+            </div>
+            <div class="controls my-4">
+              <FontAwesome
+                className="icon"
+                name="chevron-left"
+                onClick={this.previous}
+              />
+              <CarouselIndicators
+                items={items}
+                activeIndex={activeIndex}
+                onClickHandler={this.goToIndex}
+              />
+              <FontAwesome
+                className="icon"
+                name="chevron-right"
+                onClick={this.next}
+              />
+            </div>
           </Container>
         </CarouselItem>
       );
     });
 
     return (
-      <CarouselWrapper className="full-width-lg-below my-3 my-lg-5 ">
+      <CarouselWrapper className="full-width">
         <Carousel
           activeIndex={activeIndex}
           next={this.next}
@@ -230,12 +253,10 @@ class HeroCarousel extends Component {
           slide={false}
         >
           {slides}
-
         </Carousel>
       </CarouselWrapper>
     );
   }
 }
-
 
 export default HeroCarousel;
