eb371f8d1734334dcba371acb94772b127ba39b0
uday <uday@1stmain.co>
2019-09-24 22:25:48 +0530

    Fixed styles for 404 page.

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -5,22 +5,57 @@ import {RaisedButton} from './commons/';
 import emblem from "./../images/emblem.png";
 
 const Wrapper = styled.div`
+
+    & .row {
+        padding-top: 100px;
+        padding-bottom: 100px;
+    }
+
     & .embem {
         width: 50%;
         height: 50%;
     }
+
+    & .header {
+        font-size: 13px;
+        font-weight: 700;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+    }
+
+    & .text {
+        font-family: "Cormorant Garamond Medium";
+        font-size: 42px;
+        font-weight: 500;
+        letter-spacing: 1px;
+        line-height: 57px;
+    }
+
+    & .button {
+        margin-top: 30px;
+    }
 `;
 
 
-export const NotFound = ({history: push}) => (
+export const NotFound = ({history: { push }}) => (
     <Wrapper className="container">
         <div className="row justify-content-center">
-            <div className="col-12">
+            <div className="col-12 text-center">
                 <img className="emblem" src={emblem} />
             </div>
-            <div className="col-12">Error 404</div>
-            <div className="col-12">Sorry, this page was not found.</div>
-            <RaisedButton  className="col-12" onClick={() => push("/")} >Home</RaisedButton>
+            <div className="col-12 text-center">
+                <div className="header">
+                    Error 404
+                </div>
+            </div>
+            <div className="col-12 text-center">
+                <div className="text">
+                    Sorry, this page was not found.
+                </div>
+            </div>
+            <div className="col-12 text-center">
+                <RaisedButton className="button" onClick={() => push("/")} >Go to Home</RaisedButton>
+            </div>
         </div>
     </Wrapper>
 )
